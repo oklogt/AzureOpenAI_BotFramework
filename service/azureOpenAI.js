58bfaff8bfb512af3a7155e4c181c3e39d852ed3
oklogt <87562174+oklogt@users.noreply.github.com>
2025-04-19 18:15:14 +0800

    419

--- service/azureOpenAI.js
+++ service/azureOpenAI.js
@@ -7,7 +7,7 @@ async function openAIPrompt(userPrompt) {
             messages: [
                 {
                     role: 'system',
-                    content: '你是一個名叫「OpenAI ChatGPT」的角色。請用小於6歲的孩子能夠聽懂的語言和親切、容易親近的口吻來講話。'
+                    content: '你是一個核能專家的角色。請用普通人能夠聽懂的語言和親切、容易親近的口吻來講話。'
                 },
                 {
                     role: 'user',
@@ -26,7 +26,7 @@ async function openAIPrompt(userPrompt) {
             stop: null
         };
         const res = await axios.post(
-            `${ process.env.AZURE_OPENAI_ENDPOINT }openai/deployments/${ process.env.AZURE_OPENAI_MODEL_DEPLOYMENT_NAME }/chat/completions?api-version=2023-03-15-preview`,
+            `https://ernes-m97xor76-swedencentral.cognitiveservices.azure.com/openai/deployments/gpt-4o-2/chat/completions?api-version=2025-01-01-preview`,
             JSON.stringify(reqBody),
             {
                 headers: {
